9062804db3230ff1f1211145b2a6dc16d3aae4c9
kekoyana <kekoyana0@gmail.com>
2025-02-20 00:45:19 +0900

    netlifyへのデプロイを追加

--- evolife/vite.config.ts
+++ evolife/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ai_game/evolife/',
+  base: process.env.DEPLOY_TARGET === 'netlify' ? '/' : '/ai_game/evolife/',
 })
