11570b96e43e7132fe2065af3a310dfec4ca7af4
kekoyana <kekoyana0@gmail.com>
2025-03-01 00:03:28 +0900

    fix

--- gakuensai/src/App.tsx
+++ gakuensai/src/App.tsx
@@ -1,5 +1,5 @@
 import { Provider } from 'react-redux'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { store } from './features/store'
 import './App.css'
 import TitlePage from './pages/TitlePage'
--- gakuensai/vite.config.ts
+++ gakuensai/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ai_game/gakuensai/',
   plugins: [react()],
 })
