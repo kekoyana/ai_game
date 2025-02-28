fee371257b88f3acbed7d405cf433513401be759
kekoyana <kekoyana0@gmail.com>
2025-03-01 00:12:17 +0900

    画像リンク修正

--- gakuensai/src/components/Background.tsx
+++ gakuensai/src/components/Background.tsx
@@ -1,4 +1,26 @@
+import React from 'react'
 import { Location } from '../types/game'
+
+// Import all background images
+import bench from '../assets/background/bench.jpg'
+import classroom from '../assets/background/classroom.jpg'
+import ground from '../assets/background/ground.jpg'
+import library_room from '../assets/background/library_room.jpg'
+import park from '../assets/background/park.jpg'
+import rooftop from '../assets/background/rooftop.jpg'
+import school_gym from '../assets/background/school_gym.jpg'
+import staircase from '../assets/background/staircase.jpg'
+
+const backgroundImages: Record<Location, string> = {
+  bench,
+  classroom,
+  ground,
+  library_room,
+  park,
+  rooftop,
+  school_gym,
+  staircase,
+}
 
 interface BackgroundProps {
   location: Location
@@ -8,7 +30,7 @@ const Background: React.FC<BackgroundProps> = ({ location }) => {
   return (
     <div className="background">
       <img
-        src={`/src/assets/background/${location}.jpg`}
+        src={backgroundImages[location]}
         alt={`Location ${location}`}
         style={{
           width: '1024px',
--- gakuensai/src/components/Character.tsx
+++ gakuensai/src/components/Character.tsx
@@ -1,4 +1,20 @@
+import React from 'react'
 import { Expression } from '../types/game'
+
+// Import all character expressions
+import angry from '../assets/character/angry.webp'
+import blush from '../assets/character/blush.webp'
+import normal from '../assets/character/normal.webp'
+import sad from '../assets/character/sad.webp'
+import smile from '../assets/character/smile.webp'
+
+const characterImages: Record<Expression, string> = {
+  angry,
+  blush,
+  normal,
+  sad,
+  smile,
+}
 
 interface CharacterProps {
   expression: Expression
@@ -8,7 +24,7 @@ const Character: React.FC<CharacterProps> = ({ expression }) => {
   return (
     <div className="character">
       <img
-        src={`/src/assets/character/${expression}.webp`}
+        src={characterImages[expression]}
         alt={`Character ${expression}`}
         style={{
           width: '400px',
