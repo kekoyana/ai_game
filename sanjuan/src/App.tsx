5c443d37af8931ad8760176e4ca6431711e60f7d
kekoyana <kekoyana0@gmail.com>
2025-04-17 01:17:16 +0900

    deploy修正

--- sanjuan/src/App.tsx
+++ sanjuan/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { PlayerState, Role } from './store/gameStore';
 import PlayerHand from './components/PlayerHand';
